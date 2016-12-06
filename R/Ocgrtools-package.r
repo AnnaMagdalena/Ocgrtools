4cfc115c091a54ba2a3df4a3fd7da9226600e438
Anna(Ania) Magdalena Posacka <ania.posacka@gmail.com>
2016-12-05 22:51:09 -0800

    Updated info for pplot and DOCUMENTED the entore package

---
+++ R/Ocgrtools-package.r
@@ -0,0 +1,9 @@
+#' Ocgrtools: Visualize oceanographic datasets
+#'
+#' Readlily plot depth profiles of ocean properties and utilize a dataset of various
+#' properties collected as a part of an expedition in the subarctic NE Pacific
+#' (LineP cruise) that is contained in the package
+#'
+#' @name Ocgrtools
+#' @docType package
+NULL
